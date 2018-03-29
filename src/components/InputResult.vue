7568cad205e0ffef21e441b37206599f3cc60c46
Joni Laurila <joni.laurila@hotmail.com>
2018-03-29 17:01:22 +0300

    Some css rules and scoreboard

--- src/components/InputResult.vue
+++ src/components/InputResult.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="input container">
-    <h1>Lisää ottelu</h1>
-    <div class="result row">
-      {{ result.home.score }} - {{ result.away.score }}
+    <div class="top" v-if="result.home.name && result.away.name">
+      <v-score-board v-bind:home="result.home" v-bind:away="result.away" />
     </div>
-    <div class="teams row">
+    <div class="top" v-if="!result.home.name || !result.away.name">
+      <h2>Valitse joukkueet:</h2>
       <v-select v-model="result.home.name" v-bind:options.sync="homeTeams" disabled="Valitse joukkue" />
        vs.
       <v-select v-model="result.away.name" v-bind:options.sync="awayTeams" disabled="Valitse joukkue" />
@@ -33,6 +33,7 @@
 <script>
 import SelectTeam from './html/SelectTeam'
 import ListGoals from './html/ListGoals'
+import ScoreBoard from './ScoreBoard'
 
 import * as data from '../assets/teams.json'
 
@@ -40,7 +41,8 @@ export default {
   name: 'InputResult',
   components: {
     'v-select': SelectTeam,
-    'v-list-goals': ListGoals
+    'v-list-goals': ListGoals,
+    'v-score-board': ScoreBoard
   },
   data () {
     return {
@@ -105,4 +107,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .container {
+    display: grid;
+    grid-template-areas:
+      "top"
+      "middle"
+      "bottom";
+    margin: auto;
+    width: 500px;    
+  }
 </style>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="left-top">
+      <p><strong>Koti</strong></p>
+    </div>
+    <div class="middle-top" />
+    <div class="right-top">
+      <p><strong>Vieras</strong></p>
+    </div>
+    <div class="left-bot">
+      <p>{{home.name}}</p>
+    </div>
+    <div class="middle-bot">
+      <p>{{home.score}} - {{away.score}}</p>
+    </div>
+    <div class="right-bot">
+      <p>{{away.name}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    'home': {
+      'name': {
+        type: String,
+        required: true
+      },
+      'score': {
+        type: Number,
+        required: true
+      }
+    },
+    'away': {
+      'name': {
+        type: String,
+        required: true
+      },
+      'score': {
+        type: Number,
+        required: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    display: grid;
+    grid-template-areas:
+      "left-top middle-top right-top"
+      "left-bot middle-bot right-bot";
+    justify-items: center;
+    align-items: center;
+    font-size: 2em;
+  }
+
+  .left-top {
+      align-self: end;
+  }
+</style>
