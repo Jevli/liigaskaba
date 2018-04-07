417316ea74a7e6264835bf7c3e5a95d996e4e341
Joni Laurila <joni.laurila@hotmail.com>
2018-04-07 22:21:22 +0300

    Created function to load all games online, also done that

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+
+exports.importGames = functions.https.onRequest((request, response) => {
+  const games = request.body.games;
+  let done = 0
+  console.log('Games: ' + games.length)
+  games.forEach(game => {
+    db.collection('games')
+    .add(game)
+    .then(ref => {
+      done++
+      console.log('Added document with ID: ', ref.id, done);
+    }).catch(err => {
+      console.log(err)
+    });
+  });
+  response.send('Adding files')
+});
