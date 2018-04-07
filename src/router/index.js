b2f280f029da2765c6fd457a80842d444f5a1394
Joni Laurila <joni.laurila@hotmail.com>
2018-04-07 23:07:40 +0300

    Added login, rerouting and querying data from database

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
+
 import MainView from '@/components/MainView'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
+      path: '*',
+      redirect: '/login'
+    },
+    {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/logout',
+      name: 'Logout'
+    },
+    {
+      path: '/main',
       name: 'MainView',
       component: MainView,
+      meta: {
+        requiresAuth: true
+      },
       props: (route) => ({ query: { team: route.query.team, place: route.query.place } })
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (to.path === '/logout') {
+    if (currentUser !== null) firebase.auth.signOut().then(() => next('login'))
+    else next('login')
+  } else if (requiresAuth && !currentUser) next('login')
+  else next()
+})
+
+export default router
