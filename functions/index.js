a3de794ca9b35bf4a8c23759a37c1ae159bc03b0
Joni Laurila <joni.laurila@hotmail.com>
2018-04-14 17:06:12 +0300

    Added function for querying season all matches

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
+const axios = require('axios')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
 admin.initializeApp(functions.config().firebase);
 var db = admin.firestore()
+
+const baseUrl = 'http://mc.instatfootball.com/api/v1/'
+const seasons = 'seasons?tournament_id=70'
+const seasonMatchesUrl = 'matches?locale=en&tournament_id=70&season_id='
+const matchUrl = 'matches'
 
 exports.importGames = functions.https.onRequest((request, response) => {
   const games = request.body.games
@@ -19,6 +25,18 @@ exports.importGames = functions.https.onRequest((request, response) => {
     })
   })
   response.send('Adding files')
+})
+
+exports.updateSeasonMatches = functions.https.onRequest((request, response) => {
+  const id = request.body.id || 21
+  makeInstatQuery(seasonMatchesUrl + id)
+  .then((json) => {
+    response.status(200).send(json)
+  })
+  .catch((err) => {
+    console.error(err)
+    response.status(500).send('Failed to get matches')
+  })
 })
 
 exports.updateStandings = functions.firestore
@@ -147,4 +165,17 @@ const getUpadatedRecord = (prev, now) => {
   } else {
     console.error('Something went wrong')
   }
+}
+
+const makeInstatQuery = (url) => {
+  return new Promise((resolve, reject) => {
+    axios.get(baseUrl + url)
+      .then(response => {
+        console.log(response.headers)
+        resolve(response.data)
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
 }
